965eb414bea052aca7a26b2123e6ce36c30884ce
thay03 <thayzalida124@gmail.com>
2025-03-30 02:51:32 +0000

    terminar gerar laudo

--- meu-projeto/src/pages/GerarLaudo.jsx
+++ meu-projeto/src/pages/GerarLaudo.jsx
@@ -1,13 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import Sidebar from "../components/Sidebar"; 
-import "../styles/GerarLaudo.css"
+import jsPDF from "jspdf";
+import "../styles/GerarLaudo.css";
 
 function GerarLaudo() {
   const { id } = useParams();
 
-  const handleExportPDF = () => {
-    console.log("Exportando PDF do laudo...");
+  const GerarLaudo = () => {
+    const [dados, setDados] = useState({
+    numeroCaso: "",
+    data: "",
+    perito:"",
+    nomePaciente: "",
+    nascimento:"",
+    sexo:"",
+    documento:"",
+    filiacao:"",
+    localExame:"",
+    motivo:"",
+    metodos:[],
+    achados:"",
+    diagnostico:"",
+    grauCerteza:"",
+  });
+
+  const handleExportPDF = (e) => {
+    const {name, value, type, checked} = e.target;
+    if (type === "checkbox" ){
+      setDados((prev) => ({
+        ...prev,
+        metodos: checked
+        ?[...prev.metodos, value]
+        : prev.metodos.filter((m) => m !== value),
+      }));
+    } else{
+      setDados((prev) => ({...prev, [name]: value}));
+    }
+  };
+  const gerarPDF = () => {
+    const doc = new jsPDF();
+    doc.setFont("helvetica");
+    doc.setFontSize(16);
+    doc.text("LAUDO ODONTOLÓGICO PERICIAL", 20, 20);
+
+    doc.setFontSize(12);
+    doc.text(`Número do Caso: ${dados.numeroCaso}`, 20, 30);
+    doc.text(`Data: ${dados.data}`, 20, 40);
+    doc.text(`Perito Responsável: ${dados.perito}`, 20, 50);
+
+    doc.text("IDENTIFICAÇÃO DO EXAMINADO", 20, 70);
+    doc.text(`Nome: ${dados.nomePaciente}`, 20, 80);
+    doc.text(`Nascimento: ${dados.nascimento}`, 20, 90);
+    doc.text(`Sexo: ${dados.sexo}`, 20, 100);
+    doc.text(`Documento: ${dados.documento}`, 20, 110);
+    doc.text(`Filtração: ${dados.filiacao}`, 20, 120);
+
+    doc.text("INFORMAÇÃO DO EXAME", 20, 140);
+    doc.text(`Local: ${dados.localExame}`, 20, 150);
+    doc.text(`Motivo da Perícia: ${dados.motivo}`, 20, 160);
+    doc.text(`Métodos Utilizados: ${dados.metodos.join(",")}`, 20, 170);
+
+    
+    doc.text("ACHADOS PERICIAIS", 20, 190);
+    doc.text(`DESCRIÇÃO:${dados.achados}`, 20, 140);
+
+    doc.text("CONCLUÇÃO", 20, 220);
+    doc.text(`Diagnóstico: ${dados.diagnostico}`, 20, 230);
+    doc.text(`Grau de certeza: ${dados.grauCerteza}`, 20, 240);
+
+    doc.text("_______________________", 20, 260);
+    doc.text(`${dados.perito}`, 20, 270);
+    doc.text("Perito Responsável", 20, 280);
+
+    doc.save("laudo_odontologico.pdf")
   };
 
   return (
@@ -17,24 +83,46 @@ function GerarLaudo() {
 
     <div className="gerar-laudo-container">
       <h2>Geração de Laudo para Caso {id}</h2>
-      <form>
-        <label>Responsável</label>
-        <input type="text" />
-        <label>Nome do Caso</label>
-        <input type="text" />
-        <label>Descrição</label>
-        <textarea />
-        <label>Informações Adicionais</label>
-        <textarea />
-        <label>Assinatura Digital</label>
-        <input type="file" />
-        <button type="button" onClick={handleExportPDF}>
-          Exportar em PDF
-        </button>
-      </form>
+
+    <label>Número do Caso :</label>
+    <input type="text" name="numeroCaso" onChange={handleChange}></input>
+
+    <label>Data :</label>
+    <input type="date" name="data" onChange={handleChange}></input>
+
+    <label>Perito Responsável :</label>
+    <input type="text" name="perito" onChange={handleChange}></input>
+
+    <h3>Identificação do Examinado</h3>
+    <label>Nome do Paciente :</label>
+    <input type="text" name="numeroPacirnte" onChange={handleChange}></input>
+
+    <label>Data de Nascimento</label>
+    <input type="text" name="nascimento" onChange={handleChange}></input>
+
+    <label>Sexo:</label>
+    <select name="sexo" onChange={handleChange}>
+      <option value="">Selecione</option>
+      <option value="Masculino">Masculino</option>
+      <option value="Feminino">Feminino</option>
+    </select>
+
+    <label>Documento:</label>
+    <input type="text" name="documento" onChange={handleChange}></input>
+
+    <label>Filiação</label>
+    <input type="text" name="feliacao" onChange={handleChange}></input>
+
+    <h3>Informações do Exame</h3>
+    <label>Local:</label>
+    <input type="text" name="localExame" onChange={handleChange}></input>
+
+    <label>Motivos da Perícia:</label>
+    <input type="text" name="motivo" onChange={handleChange}></input>
+
     </div>
   </div>
   );
-}
+}}
 
 export default GerarLaudo;
