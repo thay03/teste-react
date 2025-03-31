c139c5efa99387dfc0f18c5ddcc8b627c14f2669
Raissa Vitória <123013855+raissabispo@users.noreply.github.com>
2025-03-31 01:09:01 +0000

    Adicionei estilo no login e no slideBar e a lógica do handleChange e ajustei funcionamento da página Gerar Laudo

--- meu-projeto/src/pages/Login.jsx
+++ meu-projeto/src/pages/Login.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "../styles/Login.css"
-import icon from "../assets/icon.webp"
+import "../styles/Login.css";
+import icon from "../assets/icon.webp";
 
 function Login() {
   const [usuario, setUsuario] = useState("");
   const [senha, setSenha] = useState("");
+  const [mostrarSenha, setMostrarSenha] = useState(false);
   const navigate = useNavigate();
 
   const handleSubmit = (e) => {
@@ -17,11 +18,12 @@ function Login() {
 
   return (
     <div className="login-container">
-       <div className="icon">
+      {/* Ícone ao lado do formulário */}
+      <div className="icon">
         <img src={icon} alt="icon-app" />
-        </div>
-      <form onSubmit={handleSubmit} className="login-from">
-       
+      </div>
+
+      <form onSubmit={handleSubmit} className="login-form">
         <label>
           Usuário:
           <input
@@ -32,15 +34,34 @@ function Login() {
             size="30"
           />
         </label>
+
         <label>
           Senha:
-          <input
-            type="password"
+          <input className="mostrarSenha"
+            type={mostrarSenha ? "text" : "password"} 
             value={senha}
             onChange={(e) => setSenha(e.target.value)}
             required
           />
         </label>
+
+    {/* Opções de senha */}
+    <div className="senha-opcoes">
+  <label className="mostrar-senha">
+    <input
+      type="checkbox"
+      checked={mostrarSenha}
+      onChange={() => setMostrarSenha(!mostrarSenha)}
+    />
+    Mostrar senha
+  </label>
+
+  <p className="esqueceu">
+    <a href="/recuperar-senha">Esqueceu a senha?</a>
+  </p>
+</div>
+
+
         <button type="submit">Entrar</button>
       </form>
     </div>
